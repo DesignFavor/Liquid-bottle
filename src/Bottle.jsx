25a18ff5657320b382a5c07f3b7fd02d2718911f
Designfavor <DesginFavor>
2024-12-04 23:08:27 -0800

    update

--- src/Bottle.jsx
+++ src/Bottle.jsx
@@ -3,9 +3,9 @@ import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
 export function Bottle({ color, labelTexture }) {
-  const { nodes, materials } = useGLTF("./model/bottle.glb");
+  const { nodes, materials } = useGLTF("/model/bottle.glb");
 
-  // Load label texture if provided
+  // Create a custom material for the label if a label texture is provided
   const labelMaterial = new THREE.MeshStandardMaterial();
   if (labelTexture) {
     const texture = new THREE.TextureLoader().load(labelTexture);
@@ -16,31 +16,50 @@ export function Bottle({ color, labelTexture }) {
     <group position={[0, -1, 0]} dispose={null}>
       <group scale={0.079}>
         {/* Bottle Top */}
-        <group position={[0, 3.015, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <mesh castShadow receiveShadow
-            geometry={nodes.Bottle_Top001.geometry}
-            material={materials["Material.002"]}
-          />
-          <mesh castShadow receiveShadow
-            geometry={nodes.Bottle_Top002.geometry}
-            material={labelTexture ? labelMaterial : materials["Bottle Top.002"]}
-          />
-        </group>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Bottle_Top001.geometry}
+          material={materials["Material.002"]}
+          position={[0, 3.015, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Bottle_Top002.geometry}
+          material={labelTexture ? labelMaterial : materials["123248_schreiner_1_pae-kirschenlikoer_1000"]}
+          position={[0.001, 6.5, 3]}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={10.5}
+        />
 
         {/* Glass Bottle */}
         <group position={[0, 3.015, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <mesh castShadow receiveShadow
+          <mesh
+            castShadow
+            receiveShadow
             geometry={nodes.Glass001.geometry}
             material={materials["Glass.003"]}
             material-color={color}
+            scale={[0.936, 0.915, 1]}
           />
-          <mesh castShadow receiveShadow geometry={nodes.Glass003.geometry} material={materials["Glass.002"]} />
-          <mesh castShadow receiveShadow geometry={nodes.Glass004.geometry} material={materials["Glass.002"]} />
-          <mesh castShadow receiveShadow geometry={nodes.Glass008.geometry} material={materials["Glass.002"]} />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Glass003.geometry}
+            material={materials["glas clear.003"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Glass008.geometry}
+            material={materials["Glass.002"]}
+          />
         </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload("./model/bottle.glb");
+useGLTF.preload("/model/bottle.glb");
